3a68bceaf1aa6ab48f25b77c2d040139fc9a5930
MikeBilbee <cmbilbee@gmail.com>
2023-03-30 11:54:33 -0500

    Mike 2

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * mon_nop - does nothing
+ * @stack: linked list stack
+ * @line_number: current line number
+ */
+void mon_nop(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+}
