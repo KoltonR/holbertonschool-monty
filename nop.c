491b60059dcbf7199d8350eaf733ef5ad497ab91
MikeBilbee <cmbilbee@gmail.com>
2023-04-02 19:06:07 -0500

    Nop

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - Do nothing
+ * @stack: Unused
+ * @line_number: Unused
+ * Return: void
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
